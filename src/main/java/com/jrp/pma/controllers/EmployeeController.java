a083b6618dc139ac88ee32e68da190a4d7d6c60e
cakirburak <cakirburak43@hotmail.com>
2024-02-16 17:46:38 +0300

    Repository, Entity, Controller and View for Employee

---
+++ src/main/java/com/jrp/pma/controllers/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.jrp.pma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.entities.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeRepository employeeRepo;
+
+	@GetMapping("/new")
+	public String showEmployeeForm(Model model) {
+		model.addAttribute("employee", new Employee());
+		return "new-employee";
+	}
+	
+	@PostMapping("/save")
+	public String showEmployeeForm(Model model, Employee employee) {
+
+		employeeRepo.save(employee);
+		return "redirect:/employees/new";
+	}
+}
