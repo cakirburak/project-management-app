634276cf5d2559f9476eff0856408ecdeaca6899
cakirburak <cakirburak43@hotmail.com>
2024-02-17 18:28:36 +0300

    get chart data end convert it to json

--- src/main/java/com/jrp/pma/controllers/HomeController.java
+++ src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.jrp.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.dto.ChartData;
 import com.jrp.pma.dto.EmployeeProject;
 import com.jrp.pma.entities.Project;
 
@@ -23,10 +28,16 @@ public class HomeController {
 	EmployeeRepository employeeRepo;
 
 	@GetMapping
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
 		List<Project> projects = (List<Project>) projectRepo.findAll();
 		List<EmployeeProject> employeesWithCnt = (List<EmployeeProject>) employeeRepo.employeeProjectsWithCount();
 
+		Map<String, Object> map = new HashMap<>();
+		List<ChartData> projectData = projectRepo.chartData();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+
+		model.addAttribute("chartData", jsonString);
 		model.addAttribute("projects", projects);
 		model.addAttribute("employeesWithCnt", employeesWithCnt);
 		return "main/home";
