013ac370afce6bcabe9c417e13a15b0cc911e2d5
cakirburak <cakirburak43@hotmail.com>
2024-02-17 19:13:24 +0300

    present data with chart UI

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,24 +1,45 @@
 const ctx = document.getElementById("myPieChart");
 
+var chartDataStr = decodeHtml(chartData);
+var chartDataJson = JSON.parse(chartDataStr)
+
+var numericData = []
+var labelData = []
+
+for(let i = 0; i < chartDataJson.length ; i++){
+	numericData[i] = chartDataJson[i].pidCount
+	labelData[i] = chartDataJson[i].projectStage
+}
+
+console.log(numericData)
+console.log(labelData)
+
+
 new Chart(ctx, {
 	type: 'pie',
 	data: {
+		labels: labelData,
 		datasets: [{
-			label: '# of Votes',
-			data: [12, 19, 3, 5, 2, 3],
-			borderWidth: 1
+			label: '',
+			data: numericData,
+			borderWidth: 1,
+			backgroundColor: [
+				'rgb(54, 162, 235)',
+				'rgb(255, 205, 86)',
+				'rgb(255, 99, 132)',
+			]
   		}]
 	},
 	options: {
-	responsive: true,
-	plugins: {
-		legend: {
-			position: 'top',
-		},
 		title: {
 			display: true,
-			text: 'Pie Chart'
+			text: 'Project Statuses'
 		}
 	}
-},
 });
+
+function decodeHtml(html){
+	var txt = document.createElement("textarea");
+	txt.innerHTML = html;
+	return txt.value
+}
