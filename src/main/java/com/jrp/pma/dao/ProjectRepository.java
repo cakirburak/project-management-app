f6c4a42bebbb27b99d16792ad148447a0706c498
cakirburak <cakirburak43@hotmail.com>
2024-02-17 18:24:49 +0300

    fetch chart data and UI update

--- src/main/java/com/jrp/pma/dao/ProjectRepository.java
+++ src/main/java/com/jrp/pma/dao/ProjectRepository.java
@@ -2,11 +2,16 @@ package com.jrp.pma.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.jrp.pma.dto.ChartData;
 import com.jrp.pma.entities.Project;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
 	public List<Project> findAll();
+
+	@Query(nativeQuery = true, value = "SELECT P.stage as projectStage, COUNT(P.project_id) as pidCount FROM project P Group by P.stage")
+	public List<ChartData> chartData();
 }
---
+++ src/main/java/com/jrp/pma/dto/ChartData.java
@@ -0,0 +1,8 @@
+package com.jrp.pma.dto;
+
+public interface ChartData {
+
+	public String getProjectStage();
+
+	public int getpidCount();
+}
