9d7f52c5d1e100d0e8cb5ecc836082f85adc7b3c
cakirburak <cakirburak43@hotmail.com>
2024-02-19 20:43:07 +0300

    Repository Implementation depending on Profile

--- src/main/java/com/jrp/pma/dao/EmployeeRepository.java
+++ src/main/java/com/jrp/pma/dao/EmployeeRepository.java
@@ -2,12 +2,16 @@ package com.jrp.pma.dao;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jrp.pma.dto.EmployeeProject;
 import com.jrp.pma.entities.Employee;
 
+@Repository
+@Profile("prod")
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 	public List<Employee> findAll();
